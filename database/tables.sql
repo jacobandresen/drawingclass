a135e67278a7b5158d4075fb698596e3c75eaf2b
Jacob Andresen <jacob.andresen@gmail.com>
2016-09-24 12:27:08 +0200

    adding adaptation (support several original images)

--- database/tables.sql
+++ database/tables.sql
@@ -1,60 +1,45 @@
--- information about the user submitting images --
-create table profile (
-  id serial,
-  firstname varchar(256),
-  lastname  varchar(256),
-  email varchar(256),
-  password varchar(256)
+﻿create table profile (
+  id 				   serial,
+  firstname 			   varchar(256) not null,
+  lastname  			   varchar(256),
+  email     			   varchar(256) not null,
+  password  			   varchar(256) not null,
+  PRIMARY KEY(id)
 );
 
--- information about which remote archive the image is coming from --
 create table archive (
-  id serial,
-  name varchar(256),
-  url varchar(256)
+  id        			   serial,
+  name      			   varchar(256) not null,
+  homepage     			   varchar(256) not null,
+  contactemail			   varchar(256) not null,
+  PRIMARY KEY(id)
  );
 
--- original image from the archive --
-create table image (
-  id serial,
-  artist varchar(256),
-  location varchar(256),
-  year  integer,
-  genre varchar(256),
-  archive_id integer,
-  url  varchar(256), -- url to access image on remote archive
-  FOREIGN KEY(archive_id) REFERENCES(archive)
-);
-
 create table user_image (
-  id serial,
-  profile_id integer,
-  image_id integer,  -- original url
-  url  varchar(256), -- url to access image on local server
-  created datetime,
-  FOREIGN KEY(user_id) references profile(id),
-  FOREIGN KEY(image_id) references image(id)
+  id				   serial,
+  profile_id			   integer,
+  title				   TEXT not null, 
+  created  		  	   timestamp default now(),
+  url 		 		   varchar(256),
+  PRIMARY KEY(id)
 );
 
--- tag to apply to an image or user image --
-create table tag (
-  id serial,
-  name varchar(256)
+create table original_image (
+  id 	     			   serial,
+  archive_id			   integer NOT null,
+  title				   TEXT not null, 
+  artist			   varchar not null,
+  year				   integer,
+  source_image_url		   varchar(256) NOT null,
+  FOREIGN KEY(archive_id) 	   REFERENCES archive(id),
+  PRIMARY KEY(id)
 );
 
--- tags applied to an image --
-create table image_tag (
-  id serial,
-  image_id integer,
-  tag_id integer,
-  FOREIGN KEY(image_id) REFERENCES image(id)
-);
-
--- tags applied to a user image --
-create table user_image_tag (
-  id serial,
-  image_id integer,
-  user_image_id integer,
-  FOREIGN KEY(user_image_id) REFERENCES user_image(id),
-  FOREIGN KEY(tag_id) REFERENCES tag(id)
+create table adaptation (
+  id 				   serial,
+  user_image_id			   integer,
+  original_image_id		   integer,
+  FOREIGN KEY (original_image_id)  REFERENCES original_image(id),
+  FOREIGN KEY (user_image_id)      REFERENCES user_image(id),
+  PRIMARY KEY(id)
 );
