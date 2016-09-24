e62dd7967d55e505fbd50db2459aa195fffa338d
Jacob Andresen <jacob@cellpointmobile.com>
2016-09-24 23:17:04 +0200

    login

--- public/index.php
+++ public/index.php
@@ -1,36 +1,31 @@
 <?php
 session_start();
+require_once('inc/db.php');
 ?>
 <html>
    <head>
       <title>Remake</title>
+      <link rel="stylesheet" href="mock/forsidestyle.css" />
    </head>
    <body>
     <?php
-    if (!isset($_SESSION['username'])) {
-       print "<a href='login.php'>log ind</a>";
-    } else {
-       print "hej ".$_SESSION['username'];
-    }
+    $query=get('query');
+    if ($query == "") $query = "picasso";
 
-    //allow facets artist,year
-    if(!isset($_GET['query'])) {
     ?>
-    <form action="index.php" method="GET">
-       <input type="text" name="query" />
-<!--       <input type="text" name="year" />
-       <input type="text" name="artist" /> -->
+    <form class="searchbar" id="searchinput" method="GET">
+       <input type="text" class="searchbar__input" placeholder="S&oslash;g kunstner" name="query" />
+       <button type="submit" class="searchbar__submit"><i class="fa fa-search " aria-hidden="true"></i></button>
     </form>
 
+ <h1 class="title">Explore</h1>
+ <div class="explore">
    <?php
-   } else {
       //show search results
-      $query=$_GET['query'];
-      if(isset($_GET['next']))
+        if(isset($_GET['next']))
           $next = $_GET['next'];
       else
    	$next = 0;
-
       $solr_string="q=$query&facet=true&fl=id, title_first,object_production_date_latest,object_production_date_earliest,medium_image_url,object_type_dk,artist_name&facet.mincount=1&start=$next&facet.limit=-1&qf=collectorExact1^150 collectorExact2^30 collectorExact3^20 collector1^20 collector2^15 collector3^10 collector4^5&facet.field=artist_surname_firstname&wt=json&defType=edismax&rows=20";
 
       # $solr_string="q=Jorn&start=0&rows=5&facet.field=artist_name";
@@ -55,41 +50,30 @@ session_start();
 	  die($nye->error);
       }
       else {
-         echo "<ul>\n";
+        // echo "<ul>\n";
 	 foreach($nye->response->docs as $cur_doc) {
              if(isset($cur_doc->medium_image_url)) {
-	         echo "<li>";
-		 echo '<a href="creator.php?id='.$cur_doc->id.'">';
-                 echo "<image src=\"$cur_doc->medium_image_url\" height=\"42\" width=\"42\" >";
+	        // echo "<li>";
+		 echo '<a class="explore__box" href="creator.php?id='.$cur_doc->id.'">';
+                 echo "<image src=\"$cur_doc->medium_image_url\" >";
 
-	         if(isset($cur_doc->artist_name)) {
-	             $nr=0;
-                     foreach($cur_doc->artist_name as $cur_artist) {
-		         if($nr==0)
-		             echo "$cur_artist";
-                         else
-			     echo ", $cur_artist";
-			$nr++;
-                     }
-                     echo ": ";
-                 }
 
-                 if(isset($cur_doc->title_first))
-                     echo "$cur_doc->title_first";
+                 //if(isset($cur_doc->title_first))
+                 //    echo "$cur_doc->title_first";
 
-                 if(isset($cur_doc->object_type_dk))
-		     echo " ($cur_doc->object_type_dk)";
+                 //if(isset($cur_doc->object_type_dk))
+		 //    echo " ($cur_doc->object_type_dk)";
 		 echo "</a>";
 
-                 echo "</li>\n";
+                 //echo "</li>\n";
  	     }
 	}
-        echo "</ul>\n";
+        //echo "</ul>\n";
     }
 
 curl_close($ch);
-
 ?>
+</div>
     <form action="index.php" method="GET">
        <input type="hidden" name="query" value="<?php echo urlencode($query) ?>" />
        <input type="hidden" name="next" value="<?php echo $next+20 ?>" />
@@ -107,11 +91,25 @@ if($next>0) {
 }
 
 
-   }
-   ?>
+if (!isset($_SESSION['userid'])) {
+
+?>
+
+<form class="login" action="login.php">
+        <h1 class="login__header">Login</h1>
+        <input class="login__inp login__inpfld" placeholder="User" type="text" name="username">
+        <input class="login__inp login__inpfld" placeholder="Password" type="password" name="password">
+        <input class="login__inp login__submit login__inpbtn" type="submit" value="Login">
+
+<div class="login__btns">
+        <a class="login__newuser login__inpbtn" href="signup.php">Ny bruger</a>
+        </div>
+</form>
+
+<?php
+}
+?>
+
    </body>
 
 </html>
-<?php
-
-
