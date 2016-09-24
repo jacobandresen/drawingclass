7509721a7b1589e3c10739e4621956e07fc7b206
Steen Thomassen <steen@thomassen.net>
2016-09-24 18:52:25 +0200

    index.php: tilføj manglede tæller

--- public/index.php
+++ public/index.php
@@ -69,6 +69,7 @@ session_start();
 		             echo "$cur_artist";
                          else
 			     echo ", $cur_artist";
+			$nr++;
                      }
                      echo ": ";
                  }
